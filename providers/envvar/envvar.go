bc76650be2016d6f4b995f629f0444e400688267
Zack Patrick <zack.patrick@outreach.io>
2022-11-17 22:02:11 -0800

    fix envvar

--- providers/envvar/envvar.go
+++ providers/envvar/envvar.go
@@ -7,25 +7,26 @@ import (
 	"github.com/zpatrick/cfg"
 )
 
+// New returns a string provider for the given environment variable.
 func New(key string) cfg.Provider[string] {
-	return &envVarProvider[string]{
+	return &provider[string]{
 		key:    key,
 		decode: func(s string) (string, error) { return s, nil },
 	}
 }
 
-// EnvVar returns a provider for the given environment variable.
+// Newf returns a provider for the given environment variable.
 // The decode function is used to convert the raw environment variable into type T.
 func Newf[T any](key string, decode func(string) (T, error)) cfg.Provider[T] {
-	return &envVarProvider[T]{key: key, decode: decode}
+	return &provider[T]{key: key, decode: decode}
 }
 
-type envVarProvider[T any] struct {
+type provider[T any] struct {
 	key    string
 	decode func(string) (T, error)
 }
 
-func (e *envVarProvider[T]) Provide(ctx context.Context) (out T, err error) {
+func (e *provider[T]) Provide(ctx context.Context) (out T, err error) {
 	val := os.Getenv(e.key)
 	if val == "" {
 		return out, cfg.NoValueProvidedError
--- providers/envvar/envvar_test.go
+++ providers/envvar/envvar_test.go
@@ -11,11 +11,57 @@ import (
 	"github.com/zpatrick/testx/assert"
 )
 
-func TestEnvVarNoValue(t *testing.T) {
-	const key = "APP_PORT"
-	os.Unsetenv(key)
+func ExampleNew() {
+	addr := cfg.Setting[string]{
+		Name:     "address",
+		Provider: envvar.New("APP_ADDR"),
+	}
 
-	provider := envvar.Newf(key, strconv.Atoi)
-	_, err := provider.Provide(context.Background())
+	addr.Get(context.Background())
+}
+
+func ExampleNewf() {
+	port := cfg.Setting[int]{
+		Name:     "port",
+		Provider: envvar.Newf("APP_PORT", strconv.Atoi),
+	}
+
+	port.Get(context.Background())
+}
+
+func TestNew_returnsProperValue(t *testing.T) {
+	const key = "TEST_KEY"
+	os.Setenv(key, "value")
+
+	val, err := envvar.New(key).Provide(context.Background())
+	assert.NilError(t, err)
+	assert.Equal(t, val, "value")
+}
+
+func TestNew_returnsNoValueProvidedError(t *testing.T) {
+	os.Clearenv()
+
+	_, err := envvar.New("TEST_KEY").Provide(context.Background())
+	assert.ErrorIs(t, err, cfg.NoValueProvidedError)
+}
+
+func TestNewf_returnsDecodedValue(t *testing.T) {
+	const key = "TEST_KEY"
+	os.Setenv(key, "value")
+
+	decoder := func(s string) (int, error) {
+		assert.Equal(t, s, "value")
+		return 1, nil
+	}
+
+	val, err := envvar.Newf("TEST_KEY", decoder).Provide(context.Background())
+	assert.NilError(t, err)
+	assert.Equal(t, val, 1)
+}
+
+func TestNewf_returnsNoValueProvidedError(t *testing.T) {
+	os.Clearenv()
+
+	_, err := envvar.Newf("TEST_KEY", strconv.Atoi).Provide(context.Background())
 	assert.ErrorIs(t, err, cfg.NoValueProvidedError)
 }
